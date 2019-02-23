f2da4a9e5e97645ce73d39101982409df21bb40d
renan <renan@aestesis.org>
2019-02-23 11:18:01 +0100

    update swift 4.2

--- Package.swift
+++ Package.swift
@@ -1,10 +1,16 @@
-// swift-tools-version:3.0.2
+// swift-tools-version:4.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "CCairo",
-    pkgConfig: "cairo",
-    providers: [.Brew("cairo"), .Apt("libcairo-dev")]
+    products: [
+        .library(name: "CCairo",targets: ["CCairo"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .systemLibrary(name:"CCairo")
+    ]
 )
